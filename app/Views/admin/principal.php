207ff54ffbd8d35f4b660b60339d52785506b9ac
CI5co22 <arnoldajcip225@gmail.com>
2025-09-01 14:51:00 -0600

    diseño base

--- app/Views/admin/principal.php
+++
@@ -1,7 +0,0 @@
-<?=  $this->extend("admin/layout"); ?>
-
-<?=  $this->section('contenido') ?>
-
-esto es principal
-<?=  $this->endSection()?>
-
--- app/Views/principal.php
+++ app/Views/principal.php
@@ -1,111 +1,131 @@
 <?=  $this->extend("layout/layout"); ?>
 
 <?=  $this->section('contenido') ?>
-
+    
 <style>
-  main section {
-    background: url('https://img.freepik.com/foto-gratis/composicion-diferentes-accesorios-coche_23-2149030439.jpg?semt=ais_hybrid');
-    background-size: cover;
-    background-color: #ffffff;
-    border-radius: 20px;
-    box-shadow: 0px 5px 15px rgba(0,0,0,0.25);
-    position: relative;
-    color: white;
-    min-height: 500px; /* Adjust this value as needed */
-  }
+    
+:root
+{
+    --principal: #685DFC;
+}
 
-  .overlay {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: rgba(0, 0, 0, 0.5);
-    border-radius: 20px;
-    display: flex;
-    align-items: flex-end;
-    justify-content: flex-start;
-    padding: 20px;
-    text-align: left;
-  }
+header h1 
+{
+  font-weight: 900;
+  color: #323554
+}
 
-  .overlay .content {
-    margin-left: 20px;
-    margin-bottom: 20px;
-    max-width: 600px; /* Adjust this value for larger or smaller content area */
-  }
 
-  h1 {
-    font-size: 3rem;
-    font-weight: 700;
-  }
+.list
+{
+  background-color: #E1E2E8;
+  border-radius: 8px; 
+  padding: 1rem;
+  margin: 10px;
+}
 
-  .box {
-    border-radius: 20px;
-    box-shadow: 0px 5px 15px rgba(0,0,0,0.25);
-    transition: .3s;
-    cursor: pointer;
-  }
+.task
+{
+  background-color: white;
+  padding: 0rem 1rem;
+  border-radius: 8px;
+  position: relative;
+}
+.task input
+{
+  width: 2rem; 
+}
 
-  .box:hover {
-    transform: scale(1.03);
-  }
+.task input:checked
+{
+  background-color: var(--principal)
+}
+.actions
+{
+   position: absolute;
+   right: 0;
+   margin: 1.3rem;
+}
 
-  .form-inline {
-    display: flex;
-    flex-direction: row;
-    gap: 0;
-    background-color: white;
-    border-radius: 10px;
-    overflow: hidden;
-  }
+.actions form .btn
+{
+  background-color: #E3E3E3;
+}
 
-  .form-inline input {
-    border: none;
-    border-radius: 0;
-    flex: 1;
-    padding: 10px;
-  }
+.actions form .btn:hover
+{
+  background-color: var(--principal);
+  color:white;
+}
 
-  .form-inline button {
-    border: none;
-    border-radius: 10px; /* Increase the border-radius of the button */
-    padding: 10px 20px;
-    margin-left: 5px;
-    font-weight: 800;
-  }
+.add, .add:hover
+{
+  background-color: var(--principal);
+  position: absolute;
+  left: 0.2rem;
+  transition: 0.3s;
+}
+
+.btn:hover
+{
+  transform: scale(1.07);
+}
+
+.top
+{
+  position: relative;
+  margin: 0 0 3rem 0;
+}
+
+.filter
+{
+  background-color: #E3E3E3;
+  position: absolute;
+  right: 0.6rem;
+  border: none;
+  color: #616161;
+}
+
+.task .task_info
+{
+  margin: 0 0 0 0.7rem;
+  text-align: left;
+}
+
+.task .task_info .date
+{
+  font-size: 14px;
+}
 </style>
+<header data-bs-theme="dark" class="mt-4 mb-4">
+  <h1>TODO LIST</h1>
+</header>
 
-<main class="bg-body-tertiary slide-top container-fluid main">
-  <section class="py-5 text-center container mt-5 mx-auto" style="max-width: 1000px;">
-    <div class="overlay">
-      <div class="content">
-        <h1 class="mb-4">Todo lo que tu auto necesita, en un solo lugar</h1>
-        <form class="form-inline w-75 p-2">
-          <input type="text" class="form-control" placeholder="Buscar repuestos">
-          <button type="submit" class="btn btn-primary"><i class="fa-solid fa-search"></i></button>
-        </form>
+<section class=" main container w-50  p-3">
+    <div class="top">
+      <button class="btn add ms-2 text-white fw-bold">Add Task</button>
+      <select class="filter form-select w-25 fw-bold" name="filter" id="">
+        <option value="">All</option>
+        <option value="">Complete</option>
+        <option value="">Incomplete</option>
+      </select>
+    </div>
+    <div class="list ">
+      <div class="task d-flex ">
+        <input type="checkbox" name="" id="">
+        <div class="task_info">
+        <p class="m-0 mt-3">Task name</p>
+        <p class="date">Fecha</p>
+        </div>
+        <div class="actions">
+          <form action="" method="post">
+            <button class="btn"><i class="fa-solid fa-edit"></i></button>
+            <button class="btn"><i class="fa-solid fa-trash"></i></button>
+          </form>
+        </div>
       </div>
     </div>
-  </section>
+</section>
 
-  <div class="album pb-5 mt-5 bg-body-tertiary" style="max-width:1000px; margin: 0 auto;">
-    <div class="container text-start pb-3">
-      <h3 class="">Compra por categoría</h3>
-    </div>
-    <div class="container" style="max-width: 1000px;">
-      <div class="row g-3">
-        <?php foreach ($cat as $row): ?>
-          <div class="col-6 col-sm-4 col-md-3 col-lg-3 mb-0">
-            <a href="./categoria/<?= $row->id ?>" style="text-decoration:none">
-              <img src="<?= base_url('img/cat.jpg') ?>" alt="producto" class="img-fluid box">
-              <p style="text-transform: capitalize; font-weight: 700; color: black;" class="container ms-2 mt-3 text-start"><?= $row->nombre ?></p>
-            </a>
-          </div>
-        <?php endforeach; ?>
-      </div>
-    </div>
-  </div>
-</main>
 
 <?=  $this->endSection() ?>
