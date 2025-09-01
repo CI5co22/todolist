e68e7df2a8e8faeb4677216d087714aada23d6a9
CI5co22 <arnoldajcip225@gmail.com>
2025-09-01 13:49:26 -0600

    cleaning
    borrando archivos extras del proyecto del que se copio CodeIgniter

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/repuestos/public';
+    public string $baseURL = 'http://localhost/todolist/public';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
