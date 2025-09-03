3f5b1d0d7acaf173b8e2038325a280a9057b2b52
CI5co22 <arnoldajcip225@gmail.com>
2025-09-03 15:21:10 -0600

    fix error de valores

--- public/js/prioridades.js
+++ public/js/prioridades.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
         
         $(this).removeClass('p1 p2 p3');
 
-        if(prioridad == 1) {
+        if(prioridad == 'Alta') {
             $(this).addClass('p1');
-        } else if(prioridad == 2) {
+        } else if(prioridad == 'Media') {
             $(this).addClass('p2');
-        } else if(prioridad == 3) {
+        } else if(prioridad == 'Baja') {
             $(this).addClass('p3');
         }
     });
