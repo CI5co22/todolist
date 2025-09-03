e929c250436553abcdc4691649445d7817178f4e
CI5co22 <arnoldajcip225@gmail.com>
2025-09-03 16:20:05 -0600

    Update principal.php

--- app/Views/principal.php
+++ app/Views/principal.php
@@ -25,7 +25,7 @@
             </form>
             <form action="./" method="GET" class="ms-1">
                 <select onchange="this.form.submit()" class="filter form-select fw-bold" name="mostrar">
-                    <option value="verTodo" selected>All</option>
+                    <option value="verTodo">All</option>
                     <option value="1">Complete</option>
                     <option value="0">Incomplete</option>
                 </select>
